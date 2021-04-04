f66bdbeb28599501b887d7301e4be119a4db6321
Akash.Paramasivan <akash.paramasivan@kanini.com>
2021-04-05 00:28:14 +0530

    Add project files.

---
+++ HomeService/Models/Booking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeService.Models
+{
+    public class Booking
+    {
+        public int Bookingid { get; set; }
+        public string CustomerId { get; set; }
+        public string ServiceProviderId { get; set; }
+        public DateTime? Servicedate { get; set; }
+        [Required(ErrorMessage = "Number working Hours cannot be empty")]
+        [Display(Name = "Expected number working Hours")]
+        public int? Starttime { get; set; }
+        public int? Endtime { get; set; }
+        public int? Estimatedcost { get; set; }
+        public bool? Bookingstatus { get; set; }
+        public bool? Servicestatus { get; set; }
+        public int? Rating { get; set; }
+       
+    }
+}
---
+++ UsersAPI/Repository/IUserRepo.cs
@@ -0,0 +1,21 @@
+﻿using UsersAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersAPI.Repository
+{
+    public interface IUserRepo
+    {
+        IEnumerable<UserServiceInfo> GetAllUsers();
+        Task<UserServiceInfo> PostUser(UserServiceInfo item);
+        UserServiceInfo GetUserById(string id);
+        UserServiceInfo GetUserServiceInfoByUserName(string Username);
+        UserServiceInfo GetUserServiceInfoByAadhaar(string Aadhaar);
+        Role GetUserRole(string username);
+        Task<UserServiceInfo> RemoveUser(string id);
+        Task<UserServiceInfo> EditUser(string id, UserServiceInfo item);
+
+    }
+}
